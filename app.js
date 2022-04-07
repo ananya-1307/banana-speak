edf3908eb5e76b2d4c22d0d073460efd5d054b45
ananya-1307 <71624430+ananya-1307@users.noreply.github.com>
2022-04-07 15:59:53 +0530

    input output processing done

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
-outputDiv.innerHTML="Translated text will come here"
+outputDiv.innerHTML="Translated text will come here "
 
     console.log(txtInput);
 
     function clickHandler()
     {
         console.log("clicked");
-        
+
         console.log("input", txtInput.value);
 
     };
